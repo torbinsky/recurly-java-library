8ecb37fa3e8fd5494a85348bd36b1b2e635ab1ce
Torben Werner <torben.werner@yupiq.com>
2013-03-19 10:56:46 -0700

    TW: Fixed license issues that maven build complains about.

--- src/main/java/com/github/torbinsky/billing/recurly/model/Invoice.java
+++ src/main/java/com/github/torbinsky/billing/recurly/model/Invoice.java
@@ -1,5 +1,6 @@
 /*
  * Copyright 2010-2012 Ning, Inc.
+ * Copyright 2013 Torben Werner - Modification to fix setter bug for line items
  *
  * Ning licenses this file to you under the Apache License, version 2.0
  * (the "License"); you may not use this file except in compliance with the
